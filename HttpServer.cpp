edd965c22bf8ffee42d05b0ce561dfbc7619aa27
Oussama Bouizgarne <obouizga@student.1337.ma>
2023-02-16 18:47:49 +0100

    Adding SetUpListeningSocket member function

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -4,21 +4,13 @@
 /* ------------------ CANONICAL FORM ------------------- */
 /* ----------------------------------------------------- */
 
-// HttpServer::HttpServer ( ARGS )
-// {
-	
-// }
+HttpServer::HttpServer ( configFileParse &config ) : _configuration(config) { }
 
-HttpServer::HttpServer ( void )
-{
-	
-}
+HttpServer::HttpServer ( void ) : _configuration(configFileParse()) { }
 
-HttpServer::~HttpServer ( void )
-{
-	
-}
+HttpServer::~HttpServer ( void ) { }
 
+/*
 HttpServer	&HttpServer::operator= ( const HttpServer &obj )
 {
 	if (this == &obj)
@@ -29,4 +21,41 @@ HttpServer	&HttpServer::operator= ( const HttpServer &obj )
 HttpServer::HttpServer ( const HttpServer &obj )
 {
 	*this = obj;
+}
+*/
+
+/* ----------------------------------------------------- */
+/* ------------------ MEMBER FUNCTIONS ----------------- */
+/* ----------------------------------------------------- */
+
+void	HttpServer::_setUpListeningSocket( void )
+{
+	struct addrinfo *bindAddress;
+	int				optval; // ???
+
+	memset(&_serverHints, 0, sizeof(_serverHints));
+	_serverHints.ai_family = AF_INET;
+	_serverHints.ai_socktype = SOCK_STREAM;
+	_serverHints.ai_flags = AI_PASSIVE;
+	
+	getaddrinfo(_configuration.serverHost, _configuration.serverPort, &_serverHints, &bindAddress);
+	
+	_listeningSocket = socket(bindAddress->ai_family, bindAddress->ai_socktype, bindAddress->ai_protocol);
+	if (_listeningSocket < 0)
+		exit (EXIT_FAILURE);
+	std::cout << "Socket Created Successfully" << std::endl;
+	optval = 1;
+    setsockopt(_listeningSocket, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+	if (bind(_listeningSocket, bindAddress->ai_addr, bindAddress->ai_addrlen))
+		exit (EXIT_FAILURE);
+	std::cout << "Binded Successfully" << std::endl;
+	freeaddrinfo(bindAddress);
+	if (listen(_listeningSocket, MAXQUEUESIZE) < 0)
+		exit (EXIT_FAILURE);
+	std::cout << "Set Up for LISTENING Successfully" << std::endl;
+}
+
+void	HttpServer::setUpHttpServer( void )
+{
+	this->_setUpListeningSocket();
 }
--- Interfaces/HttpServer.hpp
+++ Interfaces/HttpServer.hpp
@@ -1,13 +1,35 @@
 #ifndef __HTTPSERVER_H__
 # define __HTTPSERVER_H__
+# include <sys/socket.h>
+# include <netdb.h>
+# include <string.h>
+# include <iostream>
 
+# define SOCKET int
+# define MAXQUEUESIZE 10
+
+class configFileParse {
+	public:
+		char	*serverPort;
+		char	*serverHost;
+};
 class HttpServer {
 	public:
-		// HttpServer ( ARGS );
+		HttpServer ( configFileParse & );
 		HttpServer ( void );
 		~HttpServer ( void );
-		HttpServer ( const HttpServer & );
-		HttpServer	&operator= ( const HttpServer & );
+		// HttpServer ( const HttpServer & );
+		// HttpServer	&operator= ( const HttpServer & );
+		void	setUpHttpServer( void );
+	private:
+		SOCKET					_listeningSocket;
+		struct addrinfo 		_serverHints;
+		const configFileParse	_configuration;
+		void	_setUpListeningSocket ( void );
 };
 
+
+/*
+	A server listen for new connections at a publish address
+*/
 #endif
