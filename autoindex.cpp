c1887db3207cb39e8c904b218fe96279e8e8d0d1
Issam Jmari <issamjmari098@gmail.com>
2023-02-17 13:10:35 +0100

    autoindex testing

--- autoindex.cpp
+++ autoindex.cpp
@@ -1,10 +1,39 @@
+#include <set>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <algorithm>
+#include <fcntl.h>
+#include <cstdio>
 #include <dirent.h>
 
 using namespace std;
 
 int main() {
-    cout << "Content-type: text/html\n\n";
+    char *buffer = new char[1024];
+	sprintf(buffer, "HTTP/1.1 200 OK\r\n");
+	send(client->socket, buffer, strlen(buffer), 0);
+	sprintf(buffer, "Connection: close\r\n");
+	send(client->socket, buffer, strlen(buffer), 0);
+	sprintf(buffer, "Content-Length: %d\r\n", client->served_size);
+	send(client->socket, buffer, strlen(buffer), 0);
+	sprintf(buffer, "Content-Type: %s\r\n", "");
+	send(client->socket, buffer, strlen(buffer), 0);
+	sprintf(buffer, "\r\n");
+	send(client->socket, buffer, strlen(buffer), 0);
     cout << "<html><head><title>Directory Listing</title></head><body>";
     DIR *dir = opendir(".");
     if (dir == NULL) {
