a06b11c22f20c92d4b0acf797c75acf1f2530ddc
Oussama Bouizgarne <obouizga@student.1337.ma>
2023-02-23 11:50:46 +0100

    Adding more member functions to the ChunkedPostRequest class

--- Implementations/ChunkedPostRequest.cpp
+++ Implementations/ChunkedPostRequest.cpp
@@ -30,9 +30,49 @@ ChunkedPostRequest::ChunkedPostRequest ( const ChunkedPostRequest &obj )
 {
 	*this = obj;
 }
-
-
 /* ----------------------------------------------------- */
 /* ------------------ MEMBER FUNCTIONS ----------------- */
 /* ----------------------------------------------------- */
+
+void	ChunkedPostRequest::_createUploadedFile ( const char *mimeType )
+{
+	std::cout << "hola comment allez vous" << std::endl;
+	this->_uploadedFile.open(generateRandString(7) + std::string(get_real_format(mimeType)));
+}
+
+void	ChunkedPostRequest::_parseChunk( void )
+{
+	 
+}
+
+void	ChunkedPostRequest::simulatePostReq ( std::ifstream &infile )
+{
+
+	(void)infile;
+
+	std::cout << "\r\n" << std::endl;
+
+	char	buffer[BUFFER_SIZE + 1];
+
+	infile.read(buffer, BUFFER_SIZE);
+	buffer[BUFFER_SIZE] = 0;
+	std::cout << buffer << std::endl;
+	std::cout << "\n\n\n\n" ;
+	infile.read(buffer, BUFFER_SIZE);
+	buffer[BUFFER_SIZE] = 0;
+	std::cout << buffer << std::endl;
+}
+
+void	ChunkedPostRequest::handle ( const char *mimeType )
+{
+	this->_createUploadedFile(mimeType);
+	this->_receiveChunk();
+	this->_parseChunk();
+}
+
+/*
+	I should read the first chunk:
+	from where
+	ChunksHeader - 
+*/
 
--- Interfaces/ChunkedPostRequest.hpp
+++ Interfaces/ChunkedPostRequest.hpp
@@ -1,6 +1,10 @@
 #ifndef __CHUNKEDPOSTREQUEST_H__
 # define __CHUNKEDPOSTREQUEST_H__
 # include <fstream>
+# include "../Header/utils.hpp"
+# include "../errorsHandling/errorsHandling.hpp"
+# define BUFFER_SIZE 1024
+# define SOCKET int
 
 class ChunkedPostRequest {
 	public:
@@ -9,14 +13,18 @@ class ChunkedPostRequest {
 		~ChunkedPostRequest ( void );
 		ChunkedPostRequest ( const ChunkedPostRequest & );
 		ChunkedPostRequest	&operator= ( const ChunkedPostRequest & );
+		void	handleFirst( const char * );
+
+		void 	simulatePostReq ( std::ifstream & );
 	private:
 		std::ofstream	_uploadedFile;
+		SOCKET			_clientSocket;
+		char			_buffer[BUFFER_SIZE];
 		unsigned int	_currentChunkLength;
-		void			_createUploadedFile( void );
+		void			_createUploadedFile( const char * );
+		void			_receiveChunk ( void );
 		void			_parseChunk( void );
 		void			_writeToUploadedFile( void );
-			
 };
-
 
 #endif
