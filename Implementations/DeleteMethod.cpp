5f4bc0d5fa5d304ecb723ec80d4ff9a6c813afd9
Oussama Bouizgarne <obouizga@student.1337.ma>
2023-03-08 18:51:45 +0100

    RemoveRecursively and locating needed to be done

--- Implementations/DeleteMethod.cpp
+++ Implementations/DeleteMethod.cpp
@@ -4,6 +4,10 @@
 /* ------------------ CANONICAL FORM ------------------- */
 /* ----------------------------------------------------- */
 
+DeleteMethod::DeleteMethod ( std::string &path )
+	: _pathToResource(path)
+{}
+
 DeleteMethod::DeleteMethod ( void )
 {
 	
@@ -24,4 +28,42 @@ DeleteMethod	&DeleteMethod::operator= ( const DeleteMethod &obj )
 DeleteMethod::DeleteMethod ( const DeleteMethod &obj )
 {
 	*this = obj;
+}
+
+
+/* ----------------------------------------------------- */
+/* ------------------ MEMBER FUNCTIONS ----------------- */
+/* ----------------------------------------------------- */
+
+void	DeleteMethod::deleteTargetedResource ( void )
+{
+	this->_locateResource();
+	this->_deleteResource();
+}
+
+void	DeleteMethod::sendResponse( void )
+{}
+
+void	DeleteMethod::_locateResource ( void )
+{
+	this->_defineResourceType();
+	//*  proceed for location stuff 
+	if (this->_resourceType ==  FILE)
+		remove(this->_pathToResource.c_str());
+	else if (this->_resourceType == FOLDER)
+		this->removeRecursively();
+}
+
+void	DeleteMethod::_defineResourceType ( void )
+{
+	struct stat	fileStat;
+
+	if (stat(this->_pathToResource.c_str(), &fileStat))
+		throw std::runtime_error("Stat syscall failed");
+	if (S_ISREG(fileStat.st_mode))
+		this->_resourceType = FILE;
+	else if (S_ISDIR(fileStat.st_mode))
+		this->_resourceType = FOLDER;
+	else
+		this->_resourceType = UNKNOWN;
 }
--- Interfaces/DeleteMethod.hpp
+++ Interfaces/DeleteMethod.hpp
@@ -1,16 +1,27 @@
 #ifndef __DELETEMETHOD_H__
 # define __DELETEMETHOD_H__
+# include <iostream>
+# include <sys/stat.h>
+# define FOLDER 2
+# define FILE 1
+# define UNKNOWN 0
 
 class DeleteMethod {
 	public:
+		DeleteMethod ( std::string & );
 		DeleteMethod ( void );
 		~DeleteMethod ( void );
 		DeleteMethod ( const DeleteMethod & );
 		DeleteMethod	&operator= ( const DeleteMethod & );
 		void	deleteTargetedResource ( void );
+		void	sendResponse ( void );
 	private:
-
+		std::string	_pathToResource;
+		short int	_resourceType;
+		void		_locateResource ( void );
+		void		_deleteResource ( void );
+		void		_defineResourceType ( void );
+		void		_removeRecursively ( void );
 };
-
 
 #endif
